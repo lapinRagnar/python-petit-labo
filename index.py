880f5291eeea8f616b47496496e1f835a1ee4433
lapinRagnar <lapinragnar@gmail.com>
2024-03-06 19:16:22 +0100

    la suite

--- index.py
+++ index.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
-x = pd.DataFrame({'A': [1, 2, 3]})
-print(x)
+air_quality = pd.read_csv("data/air_quality_no2.csv", index_col=0, parse_dates=True)
+print("air_quality.head()", air_quality.head())
+
+air_quality.plot()
+
+
